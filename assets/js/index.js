ca056ce1e20c477d9c408b2d44ebe1888560c82e
aannadi <31830056+aannadi@users.noreply.github.com>
2018-04-24 13:10:33 -0700

    Adding index js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,43 @@
 $(document).ready(function()  {
   $('.subtitles').typeIt({
 });
+
+  $('#r').delay(4000);
+  $('#headphones').delay(4000);
+  $('#watch').delay(4000);
+
+
+
+  $('#pen').delay(4000);
+  $('#glasses').delay(4000);
+  $('#a').delay(4000);
+
+
+  $('#coffee').delay(4000);
+
+  $('#c').delay(4000);
+  $('#computer').delay(4000);
+
+  $('#p').delay(4000);
+  $('#phone').delay(4000);
+
+  $('#r').fadeIn(3000)
+  $('#headphones').fadeIn(3000)
+  $('#watch').fadeIn(3000)
+
+
+
+  $('#pen').fadeIn(3500)
+  $('#glasses').fadeIn(3500)
+  $('#a').fadeIn(3700)
+
+
+  $('#coffee').fadeIn(4000)
+
+  $('#c').fadeIn(4200)
+  $('#computer').fadeIn(4400)
+
+  $('#p').fadeIn(4700)
+  $('#phone').fadeIn(4800)
+
 } )
