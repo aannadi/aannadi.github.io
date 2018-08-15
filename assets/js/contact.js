66874660fdd99f3aea9c0904e4f4e850850c1273
aannadi <31830056+aannadi@users.noreply.github.com>
2018-08-14 20:14:50 -0700

    Fade in contact

---
+++ assets/js/contact.js
@@ -0,0 +1,12 @@
+$(document).ready(function()  {
+  $('.title').fadeIn(600);
+  $('.content').fadeIn(1300);
+  setTimeout(function()
+  {
+      $("body").toggleClass("active");
+  },
+  2200);
+
+
+
+})
